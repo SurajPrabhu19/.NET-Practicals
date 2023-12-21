d1d677f4c6c6e3f31368df0c729ad8433096ffcf
Suraj Prabhu <surajprabhu19@gmail.com>
2023-12-21 08:29:05 +0530

    Details about Response Header

--- WebApplication1/WebApplication1/Program.cs
+++ WebApplication1/WebApplication1/Program.cs
@@ -6,10 +6,22 @@ app.Run(async (HttpContext context) => {
     context.Response.Headers["TempKey"] = "this is just a demo header";
     context.Response.ContentType = "text/html";
     // text/plain, text/html, application/json, application/xml // these are the various content types you use
-    await context.Response.WriteAsync("<h1>Hello</h1>");
-    await context.Response.WriteAsync("<h2>World Bro</h2>");
+    // content length will be assigned by the kestrel server depending on the content-type
+    await context.Response.WriteAsync("<h1>Response Header</h1>");
+    await context.Response.WriteAsync("<h2>Date: Date when the response was sent</h2>");
+    await context.Response.WriteAsync("<h2>Server</h2>");
+    await context.Response.WriteAsync("<h2>Content-Type</h2>");
+    await context.Response.WriteAsync("<h2>Content-Length</h2>");
+    await context.Response.WriteAsync("<h2>Cache-Control: indicates the no of seconds that a response can be cached AT BROWSER- max-age=60</h2>");
+    await context.Response.WriteAsync("<h2>Cookie-Control: Contains cookie to send TO BROWSER- x=10</h2>");
+    await context.Response.WriteAsync("<h2>Access-Control-Allow-Origin: Used to enable CORS</h2>");
+    await context.Response.WriteAsync("<h2>Location: Contains URL to redirect</h2>");
+    await context.Response.WriteAsync("<h4>Refer MDN doc for more details</h4>");
+
 });
 //Worldpp.MapGet("/", () => "Hello World!");
 
 app.Run();
+
+
  
