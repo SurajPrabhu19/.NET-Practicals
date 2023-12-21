b35e9c2aae9bc57d4ca5c2f3fe952dde0664979e
Suraj Prabhu <surajprabhu19@gmail.com>
2023-12-21 08:18:37 +0530

    Modified Content Types as html

--- WebApplication1/WebApplication1/Program.cs
+++ WebApplication1/WebApplication1/Program.cs
@@ -4,8 +4,10 @@ var app = builder.Build();
 app.Run(async (HttpContext context) => { 
     context.Response.StatusCode = 200;
     context.Response.Headers["TempKey"] = "this is just a demo header";
-    await context.Response.WriteAsync("Hello");
-    await context.Response.WriteAsync("World Bro");
+    context.Response.ContentType = "text/html";
+    // text/plain, text/html, application/json, application/xml // these are the various content types you use
+    await context.Response.WriteAsync("<h1>Hello</h1>");
+    await context.Response.WriteAsync("<h2>World Bro</h2>");
 });
 //Worldpp.MapGet("/", () => "Hello World!");
 
