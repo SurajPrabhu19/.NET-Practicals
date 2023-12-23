1049379ac1725c7d0fdc46dfa9c229cc6ddd3b0b
Suraj Prabhu <surajprabhu19@gmail.com>
2023-12-23 22:23:54 +0530

    Read QueryString + parse it from json to string + store in variables

--- WebApplication1/WebApplication1/Program.cs
+++ WebApplication1/WebApplication1/Program.cs
@@ -14,7 +14,9 @@
 //    }
 //});
 //app.Run();
-
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Extensions.Primitives;
+using System.Net;
 
 var builder = WebApplication.CreateBuilder();
 var app = builder.Build();
@@ -24,19 +26,29 @@ app.Run(async (HttpContext context) =>
     var request = context.Request;
     var response = context.Response;
 
-    response.ContentType = "text/html";
-    //if(request.Headers.ContainsKey("AuthorizationKey"))
-    //{
-    //    var auth = request.Headers["AuthorizationKey"];
-    //    await response.WriteAsync($"<p>{auth}</p>");
-    //}
-
+    //response.ContentType = "text/html";
+    ////if(request.Headers.ContainsKey("AuthorizationKey"))
+    ////{
+    ////    var auth = request.Headers["AuthorizationKey"];
+    ////    await response.WriteAsync($"<p>{auth}</p>");
+    ////}
     StreamReader reader = new StreamReader(request.Body);
     String body = await reader.ReadToEndAsync();    // await converts a Task to String and returns it
 
     await response.WriteAsync($"<h1>{body}</h1>");
 
-
+    Dictionary<string, StringValues> queryDict = QueryHelpers.ParseQuery(body);
+    
+    if(queryDict.ContainsKey("age"))
+    {
+        Int64 id = Convert.ToInt64(queryDict["age"][0]);
+        await response.WriteAsync(id+"");
+    }
+    if(queryDict.ContainsKey("name"))
+    {
+        string name = Convert.ToString(queryDict["name"][0]);
+        await response.WriteAsync(name);
+    }
 });
 
 app.Run();
