060ed4fe253a58049716f110b91ae6959b703f29
Suraj Prabhu <surajprabhu19@gmail.com>
2023-12-26 12:31:51 +0530

    Completed Convention based routing with important comments

--- Routing Concept/Routing Concept/Controllers/HomeController.cs
+++ Routing Concept/Routing Concept/Controllers/HomeController.cs
@@ -4,9 +4,17 @@ namespace Routing_Concept.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        // https://localhost:7065/Home/Welcome
+        public IActionResult Welcome()
         {
             return View();
         }
+
+        // https://localhost:7065/Home/DisplayNumber/2
+        public int DisplayNumber(int? id)   // int? indicates that the value can be Nullable
+        {
+            return id;
+        }
+
     }
 }
--- Routing Concept/Routing Concept/Program.cs
+++ Routing Concept/Routing Concept/Program.cs
@@ -3,13 +3,18 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews(); // adding this will make it suitable for MVC
                                             //  Further, if you want Pages features in your MVC application,
                                             //  then you need to use the AddMvc() method.
+                                            // REFER: https://dotnettutorials.net/lesson/difference-between-addmvc-and-addmvccore-method/
 
 var app = builder.Build();
 
 //app.MapGet("/", () => "Hello World!");
-app.MapDefaultControllerRoute();
+//app.MapDefaultControllerRoute();
 
-//app.MapControllerRoute();
+// https://localhost:7065/ -> will call https://localhost:7065/Home/Welcome as the default url
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Welcome}/{id?}"
+    );
 
 
 
