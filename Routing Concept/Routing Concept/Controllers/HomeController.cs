9f12a54c1f2ebdf8dd0984ad630f4b645d4a45a3
Suraj Prabhu <surajprabhu19@gmail.com>
2023-12-27 13:32:01 +0530

    improving route code

--- Routing Concept/Routing Concept/Controllers/HomeController.cs
+++ Routing Concept/Routing Concept/Controllers/HomeController.cs
@@ -2,7 +2,7 @@
 
 namespace Routing_Concept.Controllers
 {
-    [Route("[controller]")]
+    [Route("[controller]/[action]")]
     public class HomeController : Controller
     {
         // https://localhost:7065/Home/
@@ -15,14 +15,13 @@ namespace Routing_Concept.Controllers
         }
 
         // https://localhost:7065/Home/Welcome
-        [Route("[action]")]
         public IActionResult Welcome()
         {
             return View();
         }
         
         // https://localhost:7065/Home/DisplayNumber/2
-        [Route("[action]/{id}")]
+        [Route("{id}")]
         public int DisplayNumber(int? id)   // int? indicates that the value can be Nullable
         {
             return id ?? 10;    // ?? checks if NULL, if NULL then return 10 else return the id
