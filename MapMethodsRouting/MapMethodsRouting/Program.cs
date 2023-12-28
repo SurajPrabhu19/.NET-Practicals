f77f61bcde458422f488da93a64e49f16be305c6
Suraj Prabhu <surajprabhu19@gmail.com>
2023-12-28 12:47:37 +0530

    Implemented Endpoints Http request methods in Program.cs

--- MapMethodsRouting/MapMethodsRouting/Program.cs
+++ MapMethodsRouting/MapMethodsRouting/Program.cs
@@ -1,14 +1,23 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-//app.MapGet("/", () => "Hello World!");
-app.Map("/Home", () => "Universal map function that takes all kind of result");
-app.MapGet("/Home", () => "Map Get Function");
-app.MapPost("/Home", () => "Map Post Function");
-app.MapPut("/Home", () => "Map Put Function");
-app.MapDelete("/Home", () => "Map Delete Function");
+//app.Map("/Home", () => "Universal map function that takes all kind of result");
+//app.MapGet("/Home", () => "Map Get Function");
+//app.MapPost("/Home", () => "Map Post Function");
+//app.MapPut("/Home", () => "Map Put Function");
+//app.MapDelete("/Home", () => "Map Delete Function");
+
 // here for each route || "/Home" || we have a delegate function i.e. || () => {"Hello"} || also know as request delegate
 
+// USING Endpoints:
+app.UseRouting();
 
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapGet("/Home", async (context) => { await context.Response.WriteAsync("Endpoint for Http GET"); });
+    endpoints.MapPut("/Home", async (context) => { await context.Response.WriteAsync("Endpoint for Http PUT"); });
+    endpoints.MapPost("/Home", async (context) => { await context.Response.WriteAsync("Endpoint for Http POST"); });
+    endpoints.MapDelete("/Home", async (context) => { await context.Response.WriteAsync("Endpoint for Http Delete"); });
+});
 
 app.Run();
