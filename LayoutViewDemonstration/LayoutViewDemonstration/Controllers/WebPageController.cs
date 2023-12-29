db4be5628a0fe023acce998f1cd02b9cc59de31e
Suraj Prabhu <surajprabhu19@gmail.com>
2023-12-29 18:58:37 +0530

    Added _Layout.cshtml

---
+++ LayoutViewDemonstration/LayoutViewDemonstration/Controllers/WebPageController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LayoutViewDemonstration.Controllers
+{
+    public class WebPageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+
+
+    }
+}
--- LayoutViewDemonstration/LayoutViewDemonstration/obj/Debug/net6.0/LayoutViewDemonstration.AssemblyInfo.cs
+++ LayoutViewDemonstration/LayoutViewDemonstration/obj/Debug/net6.0/LayoutViewDemonstration.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LayoutViewDemonstration")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+050b7911d91010d946374c363cc96fd7019dce63")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("LayoutViewDemonstration")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LayoutViewDemonstration")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
