050b7911d91010d946374c363cc96fd7019dce63
Suraj Prabhu <surajprabhu19@gmail.com>
2023-12-29 17:32:35 +0530

    Implemented Endpoints for various HTTP Request methods

--- MapMethodsRouting/MapMethodsRouting/Program.cs
+++ MapMethodsRouting/MapMethodsRouting/Program.cs
@@ -1,6 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+// USING Middlewares: ----------------------------------------------------------------------------------
 //app.Map("/Home", () => "Universal map function that takes all kind of result");
 //app.MapGet("/Home", () => "Map Get Function");
 //app.MapPost("/Home", () => "Map Post Function");
@@ -8,10 +9,12 @@ var app = builder.Build();
 //app.MapDelete("/Home", () => "Map Delete Function");
 
 // here for each route || "/Home" || we have a delegate function i.e. || () => {"Hello"} || also know as request delegate
+// -----------------------------------------------------------------------------------------------------
 
-// USING Endpoints:
+// USING Endpoints: ------------------------------------------------------------------------------------
 app.UseRouting();
 
+// these middlewares are used to implement custom based logic:
 app.UseEndpoints(endpoints =>
 {
     endpoints.MapGet("/Home", async (context) => { await context.Response.WriteAsync("Endpoint for Http GET"); });
@@ -19,5 +22,7 @@ app.UseEndpoints(endpoints =>
     endpoints.MapPost("/Home", async (context) => { await context.Response.WriteAsync("Endpoint for Http POST"); });
     endpoints.MapDelete("/Home", async (context) => { await context.Response.WriteAsync("Endpoint for Http Delete"); });
 });
+//-------------------------------------------------------------------------------------------------------
+
 
 app.Run();
