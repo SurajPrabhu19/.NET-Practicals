45ecd5d144c3e9928f7b7222c8d655c49d5ee4d0
Suraj Prabhu <surajprabhu19@gmail.com>
2023-12-31 10:08:34 +0530

    Added StudentModel to Mvc Project

---
+++ ModelsImplementation/ModelsImplementation/Controllers/StudentController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ModelsImplementation.Models;
+
+namespace ModelsImplementation.Controllers
+{
+    [Route("[controller]")]
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<StudentModel> students = new List<StudentModel>() {
+                new StudentModel{id=1, age=23, name="suraj", email="s@gmail.com"},
+                new StudentModel{id=2, age=23, name="prabhu", email="p@gmail.com"},
+                new StudentModel{id=3, age=24, name="nitya", email="s@gmail.com"},
+            };
+
+            ViewData["studs"] = students;
+
+            return View();
+        }
+    }
+}
---
+++ ModelsImplementation/ModelsImplementation/Models/StudentModel.cs
@@ -0,0 +1,12 @@
+﻿namespace ModelsImplementation.Models
+{
+    public class StudentModel
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public int age { get; set; }
+
+        public StudentModel() { }
+    }
+}
