121f46c278c2e2dec5a823de0a1d0df42431bbc6
Suraj Prabhu <surajprabhu19@gmail.com>
2023-12-31 23:28:20 +0530

    Added EmployeeModel

---
+++ ModelsImplementation/ModelsImplementation/Models/EmployeeModel.cs
@@ -0,0 +1,12 @@
+﻿namespace ModelsImplementation.Models
+{
+    public class EmployeeModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int EmployeeId { get; set; }
+        public EmployeeModel() { }
+
+    }
+}
