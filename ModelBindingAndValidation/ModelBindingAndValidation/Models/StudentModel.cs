d5a493b879046a7076dcd71284ae380c43506314
Suraj Prabhu <surajprabhu19@gmail.com>
2024-01-02 00:10:52 +0530

    Few Validations added

--- ModelBindingAndValidation/ModelBindingAndValidation/Models/StudentModel.cs
+++ ModelBindingAndValidation/ModelBindingAndValidation/Models/StudentModel.cs
@@ -5,7 +5,9 @@ namespace ModelBindingAndValidation.Models
     public class StudentModel
     {
         [Required(ErrorMessage = "We believe in having a name to a person")]
-        [MinLength(2)]
+        //[MinLength(2)]
+        //[MaxLength(8)]
+        [StringLength(15, MinimumLength = 5, ErrorMessage ="Name must be between 3 to 15 chars")]
         public string name { get; set; }
     }
 }
