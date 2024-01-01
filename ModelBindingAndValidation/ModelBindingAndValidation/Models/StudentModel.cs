ad64a23614bb5fa8301109bcbe2d9a0c31b67eb4
Suraj Prabhu <surajprabhu19@gmail.com>
2024-01-02 01:33:16 +0530

    Validation annotations added to Model attributes

--- ModelBindingAndValidation/ModelBindingAndValidation/Models/StudentModel.cs
+++ ModelBindingAndValidation/ModelBindingAndValidation/Models/StudentModel.cs
@@ -5,9 +5,15 @@ namespace ModelBindingAndValidation.Models
     public class StudentModel
     {
         [Required(ErrorMessage = "We believe in having a name to a person")]
-        //[MinLength(2)]
-        //[MaxLength(8)]
         [StringLength(15, MinimumLength = 5, ErrorMessage ="Name must be between 3 to 15 chars")]
         public string name { get; set; }
+
+        [Required(ErrorMessage = "Please create a email address for login purpose")]
+        [EmailAddress]  //  Better use Regular Expression instead of [EmailAddress]
+        public string email { get; set; }
+
+        [Required(ErrorMessage = "Please add your age")]
+        [Range(minimum: 10, maximum: 150, ErrorMessage = "We would like to hire mortal humans")]
+        public int age { get; set; }
     }
 }
