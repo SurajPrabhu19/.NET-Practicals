9368f75b6551d2a16b21925766a0c0332fd3b572
Suraj Prabhu <surajprabhu19@gmail.com>
2024-01-01 11:57:20 +0530

    Used Tag Helpers for Redirection - to other Action method

--- ModelsImplementation/ModelsImplementation/Controllers/HomeController.cs
+++ ModelsImplementation/ModelsImplementation/Controllers/HomeController.cs
@@ -28,5 +28,19 @@ namespace ModelsImplementation.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public string Display(String str)
+        {
+            return str+"";
+        }
     }
 }
