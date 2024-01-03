2c875526ebcbafaa117076e1a80ee3cd36da5ef4
Suraj Prabhu <surajprabhu19@gmail.com>
2024-01-03 08:09:24 +0530

    Setup dbcontext

--- EntityCoreFrameworkImplementation/EntityCoreFrameworkImplementation/Models/StudentDbContext.cs
+++ EntityCoreFrameworkImplementation/EntityCoreFrameworkImplementation/Models/StudentDbContext.cs
@@ -1,6 +1,14 @@
-﻿namespace EntityCoreFrameworkImplementation.Models
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EntityCoreFrameworkImplementation.Models
 {
-    public class StudentDbContext
+    public class StudentDbContext: DbContext
     {
+        public StudentDbContext(DbContextOptions options): base(options)
+        {
+            
+        }
+
+        public DbSet<StudentDbContext> Student { get; set; }    // Represents table of the database
     }
 }
--- EntityCoreFrameworkImplementation/EntityCoreFrameworkImplementation/Models/StudentModel.cs
+++ EntityCoreFrameworkImplementation/EntityCoreFrameworkImplementation/Models/StudentModel.cs
@@ -1,10 +1,16 @@
-﻿namespace EntityCoreFrameworkImplementation.Models
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EntityCoreFrameworkImplementation.Models
 {
     public class StudentModel
     {
+        [Key]
         public int id { get; set; }
+        [Column("studentname", TypeName = "varchar(100)")]
         public string name { get; set; }
-        public string email { get; set; }
+        [Column("studentname", TypeName = "varchar(100)")]
         public int age { get; set; }    
         public string gender { get; set; }
     }
