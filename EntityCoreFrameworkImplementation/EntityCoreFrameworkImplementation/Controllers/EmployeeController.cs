686b9ab8c889e5d3e2ee6e9e4ac6aef0684c8ee1
Suraj Prabhu <surajprabhu19@gmail.com>
2024-01-06 14:01:45 +0530

    Added few validations + Success Alert using bootstrap

--- EntityCoreFrameworkImplementation/EntityCoreFrameworkImplementation/Controllers/EmployeeController.cs
+++ EntityCoreFrameworkImplementation/EntityCoreFrameworkImplementation/Controllers/EmployeeController.cs
@@ -28,14 +28,18 @@ namespace EntityCoreFrameworkImplementation.Controllers
         [HttpPost]
         public async Task<IActionResult> Create(EmployeeModel emp)
         {
+            if (emp == null) return NotFound();
+
+            TempData["status"] = "Operation Unsuccessful!";
+
             if (ModelState.IsValid)
             {
                 employeeModelDbContext.Employee.Add(emp);
                 await employeeModelDbContext.SaveChangesAsync();
-                return RedirectToAction(controllerName: "Employee", actionName: "Index");
+                TempData["status"] = "Data Created";
             }
 
-            return View(emp);
+            return RedirectToAction(controllerName: "Employee", actionName: "Index");
         }
 
         [HttpGet]
@@ -49,6 +53,7 @@ namespace EntityCoreFrameworkImplementation.Controllers
         [HttpGet]
         public async Task<IActionResult> Edit(int? id)
         {
+            TempData["status"] = "Operation Unsuccessful!";
 
             if (id == null || employeeModelDbContext.Employee == null)
                 return NotFound();
@@ -62,18 +67,44 @@ namespace EntityCoreFrameworkImplementation.Controllers
         [HttpPost]
         public async Task<IActionResult> Edit(int? id, EmployeeModel employee)
         {
-
             if (id != employee.Id) return NotFound();
 
             if (ModelState.IsValid)
             {
                 employeeModelDbContext.Update(employee);
                 await employeeModelDbContext.SaveChangesAsync();
-                return RedirectToAction(controllerName: "Employee", actionName: "Index");
+                TempData["status"] = "Data Edited";
             }
+            return RedirectToAction(controllerName: "Employee", actionName: "Index");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            TempData["status"] = "Operation Unsuccessful!";
+
+            if (id == null || employeeModelDbContext.Employee == null) return NotFound();
+
+            var employee = await employeeModelDbContext.Employee.FirstOrDefaultAsync(emp => emp.Id == id);
+
+            if (employee == null) return NotFound();
+
+
             return View(employee);
         }
 
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int? id)
+        {
+            var employee = employeeModelDbContext.Employee.FirstOrDefault(emp => emp.Id == id);
 
+            if (employee != null)
+            {
+                employeeModelDbContext.Employee.Remove(employee);
+                await employeeModelDbContext.SaveChangesAsync();
+                TempData["status"] = "Data Deleted";
+            }
+            return RedirectToAction(controllerName: "Employee", actionName: "Index");
+        }
     }
 }
