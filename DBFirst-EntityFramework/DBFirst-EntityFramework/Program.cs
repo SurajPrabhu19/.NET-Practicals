49e9e8d8d6d38bc3be2f1e7cc5fcf65bc0ca9066
Suraj Prabhu <surajprabhu19@gmail.com>
2024-01-06 16:48:07 +0530

    Added services for DbContext

--- DBFirst-EntityFramework/DBFirst-EntityFramework/Program.cs
+++ DBFirst-EntityFramework/DBFirst-EntityFramework/Program.cs
@@ -1,7 +1,14 @@
+using DBFirst_EntityFramework.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+var provider = builder.Services.BuildServiceProvider();
+var config = provider.GetRequiredService<IConfiguration>();
+builder.Services.AddDbContext<ProgramentorDbFirstContext>(item => item.UseSqlServer(config.GetConnectionString("dbconn")));
 
 var app = builder.Build();
 
