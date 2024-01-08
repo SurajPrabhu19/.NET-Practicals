d0983cdafb08d794d8e0ec16340f0b47179c66cf
Suraj Prabhu <surajprabhu19@gmail.com>
2024-01-08 16:05:02 +0530

    New Project - Implementing Sessions Added a UserController with dummy actionmethods

---
+++ SessionInNetCore/SessionInNetCore/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SessionInNetCore.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            //return View();
+            return RedirectToAction("Dashboard");
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Logout()
+        {
+            return View();
+        }
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ SessionInNetCore/SessionInNetCore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SessionInNetCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
