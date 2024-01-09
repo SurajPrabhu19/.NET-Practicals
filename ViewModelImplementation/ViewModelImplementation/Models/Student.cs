4def6387189627a749cd4ca057a32324b53f2ddc
Suraj Prabhu <surajprabhu19@gmail.com>
2024-01-10 01:51:14 +0530

    Student Model added

---
+++ ViewModelImplementation/ViewModelImplementation/Models/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace ViewModelImplementation.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Std { get; set; }
+        public string email { get; set; }
+    }
+}
