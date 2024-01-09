608c5b25aaeede831d4762aaea601a1616183987
Suraj Prabhu <surajprabhu19@gmail.com>
2024-01-10 01:51:37 +0530

    Teacher Model added

---
+++ ViewModelImplementation/ViewModelImplementation/Models/SchoolViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ViewModelImplementation.Models
+{
+    public class SchoolViewModel
+    {
+        public List<Student>? Students { get; set; }
+        public List<Teacher>? Teachers { get; set; }
+    }
+}
---
+++ ViewModelImplementation/ViewModelImplementation/Models/Teacher.cs
@@ -0,0 +1,10 @@
+﻿namespace ViewModelImplementation.Models
+{
+    public class Teacher
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Gender { get; set; }
+    }
+}
