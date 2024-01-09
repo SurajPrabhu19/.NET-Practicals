53f1dc4c84c9bb3a47134ebb0c228e3cf8602483
Suraj Prabhu <surajprabhu19@gmail.com>
2024-01-10 02:22:24 +0530

    Added and displayed data using ViewModel concept

--- ViewModelImplementation/ViewModelImplementation/Controllers/HomeController.cs
+++ ViewModelImplementation/ViewModelImplementation/Controllers/HomeController.cs
@@ -15,7 +15,20 @@ namespace ViewModelImplementation.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            SchoolViewModel model = new SchoolViewModel();
+            model.Students = new List<Student>()
+            {   
+                new Student(){Id = 1, email="s@gmail.com", Name="Suraj", Std=22},           
+                new Student(){Id = 2, email="n@gmail.com", Name="Nitin", Std=23},           
+                new Student(){Id = 3, email="sp@gmail.com", Name="Prabhu", Std=23},           
+            };
+            model.Teachers = new List<Teacher>()
+            {
+                new Teacher(){Id = 1, Description="abc", Name="Suraj", Gender="Male"},           
+                new Teacher(){Id = 2, Description="def", Name="Nitin", Gender="Male"},           
+                new Teacher(){Id = 3, Description="hij", Name="Prabhu", Gender="Male"},           
+            };
+            return View(model);
         }
 
         public IActionResult Privacy()
