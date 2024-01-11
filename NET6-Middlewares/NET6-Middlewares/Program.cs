8f6d2bd8f8f0f2807ed7033c50c23c8ab232a135
Suraj Prabhu <surajprabhu19@gmail.com>
2024-01-11 12:09:52 +0530

    app.Run() middleware executed

--- NET6-Middlewares/NET6-Middlewares/Program.cs
+++ NET6-Middlewares/NET6-Middlewares/Program.cs
@@ -3,4 +3,8 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
+app.Run((context) => { return context.Response.WriteAsync("Inside Run Method"); }); 
+// run() -> basically a terminating or short-circuit extension 
+// i.e no command will run after this extension is executed
+
 app.Run();
