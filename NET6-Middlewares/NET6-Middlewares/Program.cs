87cae277b3275653971c31c11a3a65bf9af786e7
Suraj Prabhu <surajprabhu19@gmail.com>
2024-01-11 12:18:35 +0530

    app.Use() implemented

--- NET6-Middlewares/NET6-Middlewares/Program.cs
+++ NET6-Middlewares/NET6-Middlewares/Program.cs
@@ -3,8 +3,16 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
-app.Run((context) => { return context.Response.WriteAsync("Inside Run Method"); }); 
+app.Use(async (HttpContext context, RequestDelegate next) =>
+{
+    await context.Response.WriteAsync("Inside Use"); 
+    await next(context);    // next() in Use() will redirect to the next middleware else Use() will act as Run() i.e as a terminating middleware
+});
+// Use() -> this extension is used to add middleware extension that may or may not forward request to the next middleware
+
+app.Run((context) => { return context.Response.WriteAsync("Inside Run"); });
 // run() -> basically a terminating or short-circuit extension 
 // i.e no command will run after this extension is executed
+
 
 app.Run();
