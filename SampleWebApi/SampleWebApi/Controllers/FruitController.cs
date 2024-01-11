5dedf9cd571aef288fba18bf9b1e12dfea3661ae
Suraj Prabhu <surajprabhu19@gmail.com>
2024-01-11 12:05:09 +0530

    Created new Project -> Middleware implementation

---
+++ SampleWebApi/SampleWebApi/Controllers/FruitController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SampleWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FruitController : ControllerBase
+    {
+        public List<string> fruits = new List<string>()
+        {
+            "Apple", "Banana", "Kiwi", "Melon"
+        };
+
+        [HttpGet]
+        public List<string> getFruits()
+        {
+            return fruits;
+        }
+        [HttpGet("{id}")]
+        public string getFruitById(int id)
+        {
+            int len = fruits.Count;
+            if(id>=0 && id<len)
+                return fruits.ElementAt(id);
+            return "Fruit Not found";
+        }
+    }
+}
