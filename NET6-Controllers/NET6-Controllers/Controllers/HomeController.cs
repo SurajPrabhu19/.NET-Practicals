ef68b1b64343af92df8e12984f003b9b3afff13f
Suraj Prabhu <surajprabhu19@gmail.com>
2024-01-13 17:58:00 +0530

    New Controller added - HomeController.cs

---
+++ NET6-Controllers/NET6-Controllers/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NET6_Controllers.Controllers
+{
+    public class HomeController // always keep the name ending with controller for compiler to detect it as a controller
+    {
+        public string Index()
+        {
+            return "New Controller";
+        }
+    }
+}
