f2bbc8425257a465acea8d6d09599c13347d64ed
Suraj Prabhu <surajprabhu19@gmail.com>
2024-01-13 19:10:55 +0530

    Alternate way to declare a Controller class

--- NET6-Controllers/NET6-Controllers/Controllers/HomeController.cs
+++ NET6-Controllers/NET6-Controllers/Controllers/HomeController.cs
@@ -2,8 +2,10 @@
 
 namespace NET6_Controllers.Controllers
 {
+    [Controller]
     // always keep the class public -> this helps AspNetCore to instantiate object and call the action method -> this happens only if a Controller is registered in Program.cs
-    public class HomeController // always keep the name ending with controller for compiler to detect it as a controller
+    public class HomeController // either keep the name ending with controller for compiler to detect it as a controller
+                                // OR keep the [Controller] annotation at the top of class with any class name
     {
         // Attrb based routing:
         [Route("/")]
