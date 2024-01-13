dc8e1363eae9dce3f89117906f25ba446e436dc1
Suraj Prabhu <surajprabhu19@gmail.com>
2024-01-13 18:02:11 +0530

    Registering Controller/s

--- NET6-Controllers/NET6-Controllers/Program.cs
+++ NET6-Controllers/NET6-Controllers/Program.cs
@@ -1,4 +1,10 @@
+using NET6_Controllers.Controllers;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddTransient<HomeController>(); // if you want to use/register a single controller manually
+builder.Services.AddControllers(); // this service adds/registers all controllers -> add all controllers to get used by the application
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
