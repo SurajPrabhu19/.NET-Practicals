242074e0539a18aa315ea3aa5268986419ead96b
Suraj Prabhu <surajprabhu19@gmail.com>
2024-01-13 18:09:46 +0530

    Creating first attribute based route

--- NET6-Controllers/NET6-Controllers/Controllers/HomeController.cs
+++ NET6-Controllers/NET6-Controllers/Controllers/HomeController.cs
@@ -2,8 +2,11 @@
 
 namespace NET6_Controllers.Controllers
 {
+    // always keep the class public -> this helps AspNetCore to instantiate object and call the action method -> this happens only if a Controller is registered in Program.cs
     public class HomeController // always keep the name ending with controller for compiler to detect it as a controller
     {
+        // Attrb based routing:
+        [Route("/")]
         public string Index()
         {
             return "New Controller";
