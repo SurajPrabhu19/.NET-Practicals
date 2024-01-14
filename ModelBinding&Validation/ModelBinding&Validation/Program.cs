b6330f40c5dcb58e0f7b281b49a999906b92e5b7
Suraj Prabhu <surajprabhu19@gmail.com>
2024-01-14 17:17:23 +0530

    Registering Services for controller routing

--- ModelBinding&Validation/ModelBinding&Validation/Program.cs
+++ ModelBinding&Validation/ModelBinding&Validation/Program.cs
@@ -1,6 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+builder.Services.AddControllersWithViews();
+
 app.MapGet("/", () => "Hello World!");
+
+app.UseRouting();
+app.MapControllers();
 
 app.Run();
