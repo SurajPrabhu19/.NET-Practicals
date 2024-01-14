7604b4906adea1de8a8afcbc9aedd67c5720c55b
Suraj Prabhu <surajprabhu19@gmail.com>
2024-01-14 17:36:52 +0530

    model binding using [FromQuery] Only captures data from query string OR query param

--- ModelBinding&Validation/ModelBinding&Validation/Controllers/HomeController.cs
+++ ModelBinding&Validation/ModelBinding&Validation/Controllers/HomeController.cs
@@ -6,7 +6,7 @@ namespace ModelBinding_Validation.Controllers
     public class HomeController : Controller
     {
         [Route("{id?}/{isLoggedIn?}")]
-        public IActionResult Index([FromRoute]int? id, [FromRoute]bool? isLoggedIn=false)
+        public IActionResult Index([FromQuery]int? id, [FromQuery]bool? isLoggedIn=false)
         {
             if(id.HasValue == false)
                 return BadRequest("Id is Null -> Please pass some Id value");
