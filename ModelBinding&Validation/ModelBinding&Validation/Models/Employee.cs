c8b0404978a134ee5d5e340726d0a35cef5491b9
Suraj Prabhu <surajprabhu19@gmail.com>
2024-01-14 18:27:32 +0530

    Added Employee Model

---
+++ ModelBinding&Validation/ModelBinding&Validation/Models/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace ModelBinding_Validation.Models
+{
+    public class Employee
+    {
+        public int id { get; set; }
+        public string? name { get; set; }
+        public string? email { get; set; }
+        public bool? isLoggedIn { get; set; }
+    }
+}
