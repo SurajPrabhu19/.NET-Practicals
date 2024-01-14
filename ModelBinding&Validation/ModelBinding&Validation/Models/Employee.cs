fb4d0c47f58e87eb6eb090aa3d3d4b11feeb3513
Suraj Prabhu <surajprabhu19@gmail.com>
2024-01-14 19:47:55 +0530

    Added attr and toString() in Employee Model

--- ModelBinding&Validation/ModelBinding&Validation/Models/Employee.cs
+++ ModelBinding&Validation/ModelBinding&Validation/Models/Employee.cs
@@ -6,5 +6,14 @@
         public string? name { get; set; }
         public string? email { get; set; }
         public bool? isLoggedIn { get; set; }
+        public string? phoneNo { get; set; }
+        public string? password { get; set; }
+        public string? confirmPassword { get; set; }
+
+        public override string ToString()
+        {
+            return $"Employee Name: {name}\n Email:{email}\n Phone:{phoneNo } ";
+        }
     }
+
 }
