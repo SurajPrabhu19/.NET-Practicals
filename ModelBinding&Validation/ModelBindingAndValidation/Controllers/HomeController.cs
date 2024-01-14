9367ab73b5cad48247e3e3023dc6a1ceecc55c29
Suraj Prabhu <surajprabhu19@gmail.com>
2024-01-14 17:06:34 +0530

    Created New Project for Model and validation implementation

---
+++ ModelBinding&Validation/ModelBindingAndValidation/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ModelBindingAndValidation.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index(int? bookId)
+        {
+            if(!bookId.HasValue == false)
+            {
+                return BadRequest("Book id is not provided!");
+            }
+            return Content("Index Page");
+        }
+    }
+}
