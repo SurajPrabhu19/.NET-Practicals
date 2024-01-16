d5be966917c2e1b07fc75bac60c9da780116bf6b
Suraj Prabhu <surajprabhu19@gmail.com>
2024-01-16 08:41:24 +0530

    Comments added for XML

--- ModelBinding&Validation/ModelBinding&Validation/Program.cs
+++ ModelBinding&Validation/ModelBinding&Validation/Program.cs
@@ -1,5 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews().AddXmlSerializerFormatters();
+// u have to add this in case you are passing xml data in request body
+// since aspnetcore has JsonInputFormatter as the default choice
+/*
+ * Takes data from RequestBody and auto converts it into an XML:
+ * <Employee>
+ *      <name>Suraj Prabhu</name>
+ *      <email>sp@gmail.com</email>
+ * </Employee>
+ */
 
 var app = builder.Build();
 //app.MapGet("/", () => "Hello World!");
