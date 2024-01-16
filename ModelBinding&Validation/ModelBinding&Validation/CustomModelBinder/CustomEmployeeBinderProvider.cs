501579acbb7ade6e5b1c25f8188783c94a92dfa1
Suraj Prabhu <surajprabhu19@gmail.com>
2024-01-16 13:55:01 +0530

    ModelBinderProvider added for Employee Model

---
+++ ModelBinding&Validation/ModelBinding&Validation/CustomModelBinder/CustomEmployeeBinderProvider.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
+using ModelBinding_Validation.Models;
+
+namespace ModelBinding_Validation.CustomModelBinder
+{
+    public class CustomEmployeeBinderProvider : IModelBinderProvider
+    {
+        public IModelBinder? GetBinder(ModelBinderProviderContext context)
+        {
+            if(context.Metadata.ModelType == typeof(Employee))
+            {
+                return new BinderTypeModelBinder(typeof(CustomEmployeeModelBinder));
+            }
+            return null;
+        }
+    }
+}
