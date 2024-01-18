f856c67d94856152537a427931aae4dce38ef8af
Suraj Prabhu <surajprabhu19@gmail.com>
2024-01-18 13:43:01 +0530

    Completed Model Binding and Validations

---
+++ ModelBinding&Validation/ModelBinding&Validation/Progress.cs
@@ -0,0 +1,11 @@
+﻿namespace ModelBinding_Validation
+{
+    public class Progress
+    {
+        // This the list of daily progress made - includes topics that were completed without need of implementation 
+        string[] progressThatsNotImplemented =
+        {
+            "Completed Model Binding and Validations",
+        };
+    }
+}
