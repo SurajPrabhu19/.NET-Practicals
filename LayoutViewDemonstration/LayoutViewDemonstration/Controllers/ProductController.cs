7b1a8661821a4fd5d43a73a97cc042880d79582f
Suraj Prabhu <surajprabhu19@gmail.com>
2024-01-20 16:40:30 +0530

    Added Product Controller

---
+++ LayoutViewDemonstration/LayoutViewDemonstration/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LayoutViewDemonstration.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Order()
+        {
+            return View();
+        }
+        public IActionResult Search()
+        {
+            return View();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
