a52260dba2573a5072e2cdcb0eb5d4292ec54f60
Suraj Prabhu <surajprabhu19@gmail.com>
2024-01-20 16:40:19 +0530

    Added Actions in Layout Demo Controller

--- LayoutViewDemonstration/LayoutViewDemonstration/Controllers/LayoutViewDemoController.cs
+++ LayoutViewDemonstration/LayoutViewDemonstration/Controllers/LayoutViewDemoController.cs
@@ -4,7 +4,20 @@ namespace LayoutViewDemonstration.Controllers
 {
     public class LayoutViewDemoController : Controller
     {
+        [Route("/")]
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("/contact")]
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [Route("/about")]
+        public IActionResult About()
         {
             return View();
         }
