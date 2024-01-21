2e085622e0ffe85c28fad14f01ab281f5a4ce385
Suraj Prabhu <surajprabhu19@gmail.com>
2024-01-21 19:23:09 +0530

    Using EmployeeModel in About view

--- LayoutViewDemonstration/LayoutViewDemonstration/obj/Debug/net6.0/LayoutViewDemonstration.AssemblyInfo.cs
+++ LayoutViewDemonstration/LayoutViewDemonstration/obj/Debug/net6.0/LayoutViewDemonstration.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LayoutViewDemonstration")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7d87a92ff18a37dccce14dac20b61bb8f240989")]
 [assembly: System.Reflection.AssemblyProductAttribute("LayoutViewDemonstration")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LayoutViewDemonstration")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
