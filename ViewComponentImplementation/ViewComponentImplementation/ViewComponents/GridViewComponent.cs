273f20f32067ef2c7e244939210d3baa8a408051
Suraj Prabhu <surajprabhu19@gmail.com>
2024-01-22 08:17:59 +0530

    Added View Component with InvokeAsync()

---
+++ ViewComponentImplementation/ViewComponentImplementation/ViewComponents/GridViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ViewComponentImplementation.ViewComponents
+{
+    public class GridViewComponent: ViewComponent
+    {
+        public IViewComponentResult InvokeAsync()
+        {
+            // Logic
+            return View();  // invokes the Partial view Views/Shared/Components/Grid/Default.cshtml
+        }
+    }
+}
