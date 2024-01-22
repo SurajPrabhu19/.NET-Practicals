d50aefb93770b5fc8f8df04a1804aeed6f8f49b0
Suraj Prabhu <surajprabhu19@gmail.com>
2024-01-22 10:27:28 +0530

    Added Person Model

---
+++ ViewComponentImplementation/ViewComponentImplementation/Models/PersonModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ViewComponentImplementation.Models
+{
+    public class PersonModel
+    {
+        public string? Name { get; set; }
+        public string? Designation { get; set; }
+    }
+}
