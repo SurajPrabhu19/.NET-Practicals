cff9fb55aca427d938273dae86891dff0ceb374f
Suraj Prabhu <surajprabhu19@gmail.com>
2024-01-25 13:32:15 +0530

    Registered necessary Service and adding middlewares

--- DependencyInjectionImpl/DependencyInjectionImpl/Program.cs
+++ DependencyInjectionImpl/DependencyInjectionImpl/Program.cs
@@ -1,6 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+// builder.Services -> add a service in the IOC (Inversion Of Control) Container
+builder.Services.AddControllersWithViews();
+
 app.MapGet("/", () => "Hello World!");
 
+app.UseStaticFiles();
+app.UseRouting();
+app.MapControllers();
 app.Run();
