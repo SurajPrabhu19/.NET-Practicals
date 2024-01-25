4df03e1b0bb3883665aa1cde8dcb668aee92f6cd
Suraj Prabhu <surajprabhu19@gmail.com>
2024-01-25 13:35:47 +0530

    Bug fix in Program.cs

--- DependencyInjectionImpl/DependencyInjectionImpl/Program.cs
+++ DependencyInjectionImpl/DependencyInjectionImpl/Program.cs
@@ -1,12 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
-
 // builder.Services -> add a service in the IOC (Inversion Of Control) Container
 builder.Services.AddControllersWithViews();
 
-app.MapGet("/", () => "Hello World!");
+var app = builder.Build();
+
 
 app.UseStaticFiles();
 app.UseRouting();
 app.MapControllers();
+
 app.Run();
