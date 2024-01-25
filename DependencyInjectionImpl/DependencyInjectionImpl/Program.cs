e37e2afcbe4f4cf1958763fd0d89f1c49794647b
Suraj Prabhu <surajprabhu19@gmail.com>
2024-01-25 23:56:28 +0530

    Important comments added

--- DependencyInjectionImpl/DependencyInjectionImpl/Program.cs
+++ DependencyInjectionImpl/DependencyInjectionImpl/Program.cs
@@ -2,7 +2,9 @@ using Services;
 using ServicesContracts;
 
 var builder = WebApplication.CreateBuilder(args);
-// builder.Services -> add a service in the IOC (Inversion Of Control) Container
+// IOC -> builder.Services -> add a service in the IOC (Inversion Of Control) Container
+// DIP -> Dependency Inversion Principle -> 2 classes shud be linked to each other by Interface
+// DI -> Injecting or creating dependency of other service automatically
 builder.Services.AddControllersWithViews();
 builder.Services.Add(new ServiceDescriptor(
     typeof(ICityService), typeof(CityService), ServiceLifetime.Transient));
