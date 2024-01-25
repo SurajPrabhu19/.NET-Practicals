1b148836d5c33f5549d1eca8f889462944c6ba72
Suraj Prabhu <surajprabhu19@gmail.com>
2024-01-25 13:15:50 +0530

    Added CityService class

---
+++ DependencyInjectionImpl/Services/CityService.cs
@@ -0,0 +1,18 @@
+﻿namespace Services
+{
+    public class CityService
+    {
+        private List<string> _cities;
+        public CityService() { 
+            _cities = new List<string>()
+            {
+                "Mumbai", "Kolkata", "Delhi", "Banglore"
+            };
+        }
+
+        public List<string> getCities()
+        {
+            return _cities;
+        }
+    }
+}
