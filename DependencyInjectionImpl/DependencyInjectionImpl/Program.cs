e5e0d26c9e0ebed2c266666cdf01483313fda84a
Suraj Prabhu <surajprabhu19@gmail.com>
2024-01-27 19:16:05 +0530

    Configuring Autofac container + add a new service with lifetime in the Autofac container

--- DependencyInjectionImpl/DependencyInjectionImpl/Program.cs
+++ DependencyInjectionImpl/DependencyInjectionImpl/Program.cs
@@ -6,23 +6,28 @@ using ServicesContracts;
 var builder = WebApplication.CreateBuilder(args);
 
 
-// Service Provider Factory is a representation for IOC Factory
+// Service Provider Factory is a representation for default IOC Container
 builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());    // successfully changing from default to AutoFac IOC Container
 
+// only for user defined services
 builder.Host.ConfigureContainer<ContainerBuilder>(containerBuilder => { 
     containerBuilder.RegisterType<CityService>().As<ICityService>().InstancePerDependency();       // adding Transient Service
 });
+
 
 // IOC -> builder.Services -> add a service in the IOC (Inversion Of Control) Container
 // DIP -> Dependency Inversion Principle -> 2 classes shud be linked to each other by Interface
 // DI -> Injecting or creating dependency of other service automatically
 builder.Services.AddControllersWithViews();
+
 //builder.Services.Add(new ServiceDescriptor(
 //    typeof(ICityService), typeof(CityService), ServiceLifetime.Transient));
 //builder.Services.Add(new ServiceDescriptor(typeof(ICityService), typeof(CityService), ServiceLifetime.Scoped)); // adding a service with scoped service lifetime
+//  ------------------ BELOW OR ABOVE both are doing the same thing ----------------------------
 builder.Services.AddTransient<ICityService, CityService>(); // can keep one at a time to avoid comfusion
 //builder.Services.AddScoped<ICityService, CityService>();
 //builder.Services.AddSingleton<ICityService, CityService>();
+
 var app = builder.Build();
 
 
