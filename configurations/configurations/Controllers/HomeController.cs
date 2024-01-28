ecfeb5deffe822be2d6922a68b8ac8a3fb9272f8
Suraj Prabhu <surajprabhu19@gmail.com>
2024-01-28 19:53:08 +0530

    Fetch data from appsetting.json from Controller

--- configurations/configurations/Controllers/HomeController.cs
+++ configurations/configurations/Controllers/HomeController.cs
@@ -4,8 +4,16 @@ namespace configurations.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IConfiguration _configuration;
+
+        public HomeController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
         public IActionResult Index()
         {
+            ViewBag.MyKey = _configuration["MyKey"];
+            ViewBag.RandomKey = _configuration.GetValue<string>("RandomKey", "Value doesnt exists -> default value added");
             return View();
         }
     }
