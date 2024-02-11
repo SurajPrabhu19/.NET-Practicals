95df40d42bf63ec115297165301a2ca552301a49
Suraj Prabhu <surajprabhu19@gmail.com>
2024-02-11 20:13:00 +0530

    pusing few changes

--- configurations/StocksApp/Program.cs
+++ configurations/StocksApp/Program.cs
@@ -3,6 +3,7 @@ using StocksApp.Services;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddHttpClient();
+builder.Services.AddHttpContextAccessor();
 builder.Services.AddScoped<StockService>();
 
 var app = builder.Build();
--- configurations/StocksApp/Services/StockService.cs
+++ configurations/StocksApp/Services/StockService.cs
@@ -1,5 +1,7 @@
 ﻿using StocksApp.ServiceContracts;
+using System.Net.Http.Json;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace StocksApp.Services
 {
@@ -27,6 +29,12 @@ namespace StocksApp.Services
                 string responseBody = await streamReader.ReadToEndAsync();
 
                 Dictionary<string,object>? dictionary = JsonSerializer.Deserialize<Dictionary<string, object>>(responseBody);
+                //var obj = JsonSerializer.Deserialize<object>(responseBody);
+                // handling the edge case:
+                if (dictionary == null)
+                    throw new InvalidOperationException("No response from Finhub API call");
+                if (dictionary.ContainsKey("error"))
+                    throw new InvalidOperationException("Error while fetching data from Stock API");
 
                 return dictionary;
             }
