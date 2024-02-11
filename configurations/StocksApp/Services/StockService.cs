f7c6029403b4437b98b4c8b378c4bc184bf7186e
Suraj Prabhu <surajprabhu19@gmail.com>
2024-02-11 10:06:15 +0530

    Created new Service + added logic

---
+++ configurations/StocksApp/Services/StockService.cs
@@ -0,0 +1,24 @@
+﻿namespace StocksApp.Services
+{
+    public class StockService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public StockService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task Method()
+        {
+            using(HttpClient httpClient = _httpClientFactory.CreateClient())
+            {
+                HttpRequestMessage requestMessage = new HttpRequestMessage()
+                {
+                    RequestUri = new Uri("URL"),
+                    Method = HttpMethod.Get,
+                };
+                HttpResponseMessage responseMessage = await httpClient.SendAsync(requestMessage);
+            }
+        }
+    }
+}
